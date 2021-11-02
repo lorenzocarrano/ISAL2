2262d80739d08a0c8fefd0ab032c807fee38a0a6
Dilillo Nicola <nicoladilillo98@gmail.com>
2021-11-02 12:48:36 +0100

    Prototype of multiplicator

---
+++ prototype/main.c
@@ -0,0 +1,38 @@
+#include  "stdio.h"
+#include  "stdlib.h"
+
+int main(int argc, char **argv) {
+
+    FILE *fp_in = NULL;
+    FILE *fp_out = NULL;
+
+    unsigned int x1, x2;
+    void *f1;
+    void *f2;
+
+    fp_in = fopen("../fp_samples.hex", "r");
+    if (fp_in == NULL) {
+        printf("Error: cannot open file\n");
+        exit(2);
+    }
+
+    fp_out = fopen("result.txt", "w");
+
+    /// get factor
+    do{
+        fscanf(fp_in, "%x", &x1);
+        fscanf(fp_in, "%x", &x2);
+        f1 = &x1;
+        f2 = &x2;
+        //ieee 754 conversion
+        fprintf(fp_out,"0x%x (%.16f) * 0x%x (%.16f)\n", x1, *((float *) f1), x2, *((float *) f2));
+        fprintf(stdout,"0x%x (%.16f) * 0x%x (%.16f)\n", x1, *((float *) f1), x2, *((float *) f2));
+    } while (!feof(fp_in));
+
+    fclose(fp_in);
+    fclose(fp_out);
+
+    return 0;
+
+
+}
